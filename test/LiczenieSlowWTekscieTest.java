0638c45852ee8bd977284f3a8c714de2d09951f0
Stanislaw Malewicz <szeliga71@wp.pl>
2024-04-02 01:44:09 +0200

    02.04.24 /4

---
+++ test/LiczenieSlowWTekscieTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import zad3.LiczenieSlowWTekscie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LiczenieSlowWTekscieTest {
+
+    LiczenieSlowWTekscie lstTest=new LiczenieSlowWTekscie();
+
+    @Test
+    public void chectForGivenText() {
+
+        String testText = "Ala ma kota,kota ma Ala";
+
+        Map<String, Integer> testMap = new HashMap<>();
+        testMap.put("Ala", 2);
+        testMap.put("ma",2);
+        testMap.put("kota",2);
+
+        Assertions.assertEquals(lstTest.countWords(testText), testMap);
+
+    }
+
+}
---
+++ test/SortowanieListyTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import zad4.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class SortowanieListyTest {
+
+    @Test
+    public void porownanieTablic(){
+
+        Student st1 = new Student(23, 1.82, "Jan", "Kowalski");
+        Student st2 = new Student(19, 1.89, "Piotr", "Nowak");
+        Student st3 = new Student(22, 1.79, "Eryk", "Ptasinski");
+        Student st4 = new Student(24, 1.94, "Mateusz", "Zych");
+        Student st5 = new Student(27, 2.00, "Bartosz", "Kaczmarek");
+        Student st6 = new Student(25, 1.65, "Matylda", "Niewiadomska");
+        Student st7 = new Student(20, 1.69, "Julia", "Niewiadomska");
+
+        List<Student> studentsTestBefore = Arrays.asList(st1, st2, st3, st4, st5, st6,st7);
+        List<Student> studentsTestAfter=new ArrayList<>(studentsTestBefore);
+        //Collections.sort(studentsTestAfter);
+
+
+
+        Assertions.assertArrayEquals(studentsTestBefore.toArray(), studentsTestAfter.toArray());
+    }
+}
